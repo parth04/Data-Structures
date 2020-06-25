31dad490f0902249a9dc743dceaf2021a91249a5
parth04 <parth.gun@gmail.com>
2020-06-25 13:59:46 -0400

    Update sumOfLeftLeaves.py
    Instead of Left Leaves Sum by mistake added Finding Bottom Left Tree Value.
    Deleted that now.

--- Trees/sumOfLeftLeaves.py
+++ Trees/sumOfLeftLeaves.py
@@ -26,7 +26,7 @@ class Solution:
             return 0
         q = deque()
         add = 0
-        q.appendleft(root)
+        q.append(root)
 
         while q:
             front = q.popleft()
@@ -38,36 +38,7 @@ class Solution:
                 q.append(front.right)
         return add  
     
-"""
-Approach 2
-(Instead of using 2 while loop only single loop is used.
-Also, instead of list deque is used. It is very efficient.
-as deque provides an O(1) time complexity for append and 
-pop operations as compared to list which provides O(n) 
-time complexity.)
 
-
-import collections
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def findBottomLeftValue(self, root: TreeNode) -> int:
-        q= deque()
-        q.append(root)
-        res=0
-        while q:
-            front = q.popleft()
-            res =front.val
-            if front.right:
-                q.append(front.right)
-            if front.left:
-                q.append(front.left)  
-        return res
-"""
 
 
 """
